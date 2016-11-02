22e9267dc66badceedb60619b68aca6603fc950c
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-11-02 16:44:44 -0400

    add update child component of new question

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
+  updateRentalForm: false,
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    update(question, params) {
+      this.sendAction('update', question, params);
     }
   }
 });
