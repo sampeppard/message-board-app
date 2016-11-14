ce711546034127258b72913aebb48b7eb3c8b8d8
sp <sampeppard@gmail.com>
2016-11-14 00:37:38 -0500

    add computed property to display answer details

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
+    author-rating: Ember.computed('answer.author', 'answer.rating', function() {
+      return 'answered by: ' this.get('answer.author') + ' | rating: ' + this.get('answer.rating');
+    }),
+
     delete(answer) {
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyAnswer', answer);
