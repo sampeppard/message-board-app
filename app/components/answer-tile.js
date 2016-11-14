267394939386d48da4fb67d859ff9119b085ba22
sp <sampeppard@gmail.com>
2016-11-14 16:16:36 -0500

    fix unexpected token in answer tile and display

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    author-rating: Ember.computed('answer.author', 'answer.rating', function() {
-      return 'answered by: ' this.get('answer.author') + ' | rating: ' + this.get('answer.rating');
-    }),
 
+  details: Ember.computed('answer.author', 'answer.rating', function() {
+    return this.get('answer.author') + ' rating: ' + this.get('answer.rating');
+  }),
+
+  actions: {
     delete(answer) {
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyAnswer', answer);
@@ -13,3 +14,20 @@ export default Ember.Component.extend({
     }
   }
 });
+
+// import Ember from 'ember';
+//
+// export default Ember.Component.extend({
+//
+//   heading: Ember.computed('review.author', 'review.rating', function() {
+//     return this.get('review.author') + ' - ' + this.get('review.rating');
+//   }),
+//
+//   actions: {
+//     delete(review) {
+//       if (confirm('Are you sure you want to delete this review?')) {
+//         this.sendAction('destroyReview', review);
+//       }
+//     }
+//   }
+// });
