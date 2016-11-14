185220ce4c557d32d90e923bee857ba1e33e7b02
sp <sampeppard@gmail.com>
2016-11-14 17:08:15 -0500

    add functionality to display favorites

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   details: Ember.computed('answer.author', 'answer.rating', function() {
-    return this.get('answer.author') + ' rating: ' + this.get('answer.rating');
+    return this.get('answer.author') + ' | rating: ' + this.get('answer.rating');
   }),
 
   actions: {
@@ -14,20 +14,3 @@ export default Ember.Component.extend({
     }
   }
 });
-
-// import Ember from 'ember';
-//
-// export default Ember.Component.extend({
-//
-//   heading: Ember.computed('review.author', 'review.rating', function() {
-//     return this.get('review.author') + ' - ' + this.get('review.rating');
-//   }),
-//
-//   actions: {
-//     delete(review) {
-//       if (confirm('Are you sure you want to delete this review?')) {
-//         this.sendAction('destroyReview', review);
-//       }
-//     }
-//   }
-// });
